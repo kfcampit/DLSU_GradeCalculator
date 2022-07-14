1a9afe88b2bf1b50f220fd17cf618107a0f46aba
kfcampit <75428470+kfcampit@users.noreply.github.com>
2022-07-14 16:57:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DLSU_GradeCalculator
+ 
---
+++ dlsu_grade_calculator/android/app/src/main/kotlin/com/example/dlsu_grade_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dlsu_grade_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
